0296cc44067dea21a6b69f0ebcb7f82cce97d890
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-09-09 16:27:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
+
 function PageTransitions(){
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
